e52dc79e19c6a5f2b2dc14f1040522a6456fdf4b
adrifern48 <adrifern48@gmail.com>
2020-09-19 15:13:19 -0700

    set up basic structure of app

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import './App.css';
+import MainContainer from "./components/MainContainer";
 
 const App: React.FunctionComponent = () => {
   return (
     <div className="App">
       <header className="App-header">
-        
+        <MainContainer />
       </header>
     </div>
   );
---
+++ frontend/src/components/MainContent.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const MainContent: React.FunctionComponent = () => {
+  return (
+    <p>there will be switch case logic here to swap out content based on context</p>
+  );
+};
+
+export default MainContent;
